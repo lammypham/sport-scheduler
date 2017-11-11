f1d88065104d704a9e02fd194ac129a15969bfe9
Lam Pham <lammypham@gmail.com>
2017-11-11 14:59:39 -0600

    setting files up for sass then webpack dashboard

--- app/components/dashboard.jsx
+++ app/components/dashboard.jsx
@@ -7,10 +7,10 @@ export default class Dashboard extends React.Component {
                 <div className="col-lg-12">
                     <h1>Sport Scheduler Nav Bar</h1> {/*add here for now, should change to component*/}
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="col-lg-6">
                             <h2>dashboard pt1</h2>
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-lg-6">
                             <h1>dashboard pt2</h1>
                         </div>
                     </div>
